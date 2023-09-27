2eced9041189317c85fc267570d4b2cab6d8003a
Tyuiu.MedvedevKA@outlook.com <medvedevkirill2016@mail.ru>
2023-09-27 16:37:24 +0500

    Task6

---
+++ Tyuiu.MedvedevKA.Sprint1.Task6.V18.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MedvedevKA.Sprint1.Task6.V18.Lib;
+
+namespace Tyuiu.MedvedevKA.Sprint1.Task6.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            string value = "122";
+            DataService ds = new DataService();
+            bool res = ds.CheckNumber(value);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
